2e68e44c3e95c457ab9d02071cf5ed64cbd04b6e
shadab <shad46243@gmail.com>
2024-08-12 14:38:47 +0530

    reinitialize repo for spring boot

--- internationalisation/dutch.js
+++
@@ -1,7 +0,0 @@
-export const element1 = "Modern"
-
-export const element2 = "Landen"
-
-export const element3 = "Bladzijde"
-
-export const placeholder = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, tempora. Quis provident animi eius, porro laudantium omnis praesentium tempora obcaecati saepe enim possimus voluptatum, veniam consequuntur fugit repudiandae fugiat ab."
